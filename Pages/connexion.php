b8e4e4ad2be0df5f509858c0294c2d7b3853fd98
unknown <noah.crochat@gmail.com>
2022-02-15 11:03:50 +0100

    initialisation

---
+++ Pages/connexion.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../CSS/styles.css" rel="stylesheet"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+                     
+    <form class="box" method="POST" action="#">
+
+        <h2><strong><em> Authentification</em></strong></h2>
+        <br>
+        <div class="row">
+            <input id="login" type="text" name="login" placeholder ="identifiant" size="30" maxlength="45" required>
+        </div>
+        <div class="row">
+            <input id="mdp"  type="password"  name="mdp" placeholder ="mot de passe" size="30" maxlength="45" required>
+        </div>
+        <p>mot de passe oublié ?<a href="#"> Cliquez ici !</a></p>
+        <br>
+        <center><hr id="barre"></center>
+            <input type="submit" value="Valider" name="valider">
+        <p>Vous avez pas encore de compte ?<a href="#"> Inscrivez-vous ici !</a></p> 
+    </form>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="./CSS/styles.css" rel="stylesheet"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+    <body>
+        <!-- Navigation -->
+        <header>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">Je Remplis mes Bourses</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Demande de Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mes Recherches</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" style="border-radius:50px" href="./Pages/connexion.php"><button>Se Connecter</button></a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+
+        </nav>
+        </header>
+
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+
+        <br><br>
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <div class="carre">
+                    
+                    <br>
+                    <div class="container_service">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="case_img">
+                                    <img src="./Img/36b1a6da822d3b0d4eaef522260a8431.jpg">
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="row">
+                                    <p>Titre de l'annonce</p>
+                                </div>
+                                <div class="row">
+                                    <p>Prix : </p>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <p>Description : Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre . Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container_service">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="case_img">
+                                    <img src="./Img/36b1a6da822d3b0d4eaef522260a8431.jpg">
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="row">
+                                    <p>Titre de l'annonce</p>
+                                </div>
+                                <div class="row">
+                                    <p>Prix : </p>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <p>Description : Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre . Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container_service">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="case_img">
+                                    <img src="./Img/36b1a6da822d3b0d4eaef522260a8431.jpg">
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="row">
+                                    <p>Titre de l'annonce</p>
+                                </div>
+                                <div class="row">
+                                    <p>Prix : </p>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <p>Description : Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre . Généralement, on utilise un texte en faux latin, le Lorem ipsum ou Lipsum.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
