8462eabf0355962e92ee8e469c7e9891660e45e8
HerbulotV <70969050+HerbulotV@users.noreply.github.com>
2022-02-15 14:04:48 +0100

    connexion2

--- ReactNative/hackathon/components/Search.js
+++ ReactNative/hackathon/components/Search.js
@@ -7,9 +7,9 @@ class Search extends React.Component {
   render() {
     return (
       <View style={{marginTop: 50}}>
-        <Text style={{marginTop:40, fontSize:30}}>Connexion</Text>
-        <TextInput style={{marginLeft:10, marginRight: 10, height: 100, borderColor: 'black', borderWidth: 1, paddingLeft:5}} placeholder='Login'/>
-        <TextInput style={{marginLeft:10, marginRight: 10, height: 100, borderColor: 'black', borderWidth: 1, paddingLeft:5}} placeholder='Password'/>
+        <Text style={{marginTop:40, fontSize:30, color:'blue', marginLeft:120}}>Connexion</Text>
+        <TextInput style={{marginLeft:10, marginRight: 10, height:50, borderColor: 'black', borderWidth: 1, paddingLeft:5}} placeholder='Login'/>
+        <TextInput style={{marginLeft:10, marginRight: 10, height:50, borderColor: 'black', borderWidth: 1, paddingLeft:5}} placeholder='Password'/>
         <Button title='Rechercher' onPress={() => {}}/>
       </View>
     )
