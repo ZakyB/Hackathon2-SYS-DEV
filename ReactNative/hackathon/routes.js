c516da599a6b05eae53b68ba8a69f80201354f90
B. Zaky <64563717+ZakyB@users.noreply.github.com>
2022-02-16 10:46:06 +0100

    jsona reparer

--- ReactNative/hackathon/routes.js
+++ ReactNative/hackathon/routes.js
@@ -1,4 +1,6 @@
 var mysql      = require('mysql');
+const express  = require("express");
+const app      = express();
 var connection = mysql.createConnection({
   host     : 'mysql-69153-0.cloudclusters.net',
   user     : 'hackathon',
@@ -9,15 +11,26 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT * FROM `service`', function (error, results, fields) {
+connection.query('SELECT * FROM service', function (error, results, fields) {
   if (error) throw error;
-  console.log('The solution is: ', results[0]);
+  console.log('The solution is: ', results);
 });
-export function getServices(test) {
-  connection.query('SELECT * FROM `service`', function (error, results, fields) {
-    if (error) throw error;
-    return results;
+
+
+app.get("/app/test",(req, res) => {
+  res.send("hello world");
+});
+
+app.get("/",(req,res) => {
+    const sqlSelect = "SELECT * from utilisateur";
+    connection.query(sqlSelect,(err,result) => {
+    res.send(result);
   });
-}
+});
+
+app.listen(3001, () => {
+  console.log("running on port 3001");
+});
+
 
 connection.end();
