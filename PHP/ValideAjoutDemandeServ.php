9078fe0ac6fe33cae274805c1a838ca0d9284ec1
Syndar <49551286+Syndar@users.noreply.github.com>
2022-02-16 15:32:34 +0100

    ajout lesDemandesServices

---
+++ PHP/ValideAjoutDemandeServ.php
@@ -0,0 +1,31 @@
+<?php 
+session_start();
+include ("./cnx.php");
+
+if (isset($_POST['saveDemande'])){
+    if(!empty($_REQUEST['titre']) and !empty($_REQUEST['description'])){
+        $login = $_SESSION['login'];
+
+        $sql = $cnx->prepare('SELECT id,adresse from utilisateur where login=?');
+        $sql->execute([$login]); 
+        $user = $sql->fetch();
+
+        $idUtilisateur = $user[0];
+        $titre = htmlspecialchars($_REQUEST['titre']);
+        $description = htmlspecialchars($_REQUEST['description']);
+        $type = 1;
+
+        $sql2 = "insert into service (idUtilisateur,Description,titre,type) 
+        Values ('$idUtilisateur','$description','$titre','$type')";
+        $res = $cnx->prepare($sql2);
+        $res->execute();
+        header('Location:../index.php');
+
+    }else{
+        echo "Veuillez compléter tous les champs...";
+    }
+}
+
+
+
+?>
--- Pages/ajoutDemandeServices.php
+++ Pages/ajoutDemandeServices.php
@@ -9,62 +9,53 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <a href="https://icons8.com/icon/77vA1n1-oWl1/garage-fermé"></a>
 </head>
-    <body>
+<body>
         <!-- Navigation -->
-        <header>
+    <header>
         <nav class="navbar navbar-expand-lg navbar-green bg-blue fixed-top">
             <div class="container">
                 <a class="navbar-brand" href="../index.php"><img src="https://img.icons8.com/doodle/48/000000/garage-closed.png"/></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Services proposés</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">mes Recherches</a>
-                    </li>
-                    <li class="nav-item">
-                        
-                        <a class="nav-link" href="#">mon profil</a>
-                    </li>
-
-                </ul>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services proposés</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">mes Recherches</a>
+                        </li>
+                        <li class="nav-item">  
+                            <a class="nav-link" href="#">mon profil</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
-        </header>
-        <br>
-        <br>
-        <br>
-        <h2><center><strong> Demande de services</strong></center></h2>    
-        <form>
-    <center>
-        <div class="form-row ">
-    <div class="form-group col-md-4">
-        
-      <label for="titreService"></label>
-      
-      <input type="text" class="form-control" id="Service" placeholder="Titre du service">
-      
-    <br>
-    <label for="descriptionService"></label>
-
-<textarea id="Description" name="Description du service"
-          rows="4" cols="30">
-Le service .....
-</textarea>
+    </header>
     <br>
     <br>
-  <button type="submit" class="btn btn-primary">Valider la demande de Service</button>
-  </div>
-</div>
-</center>
-</form>
-
-
+    <br>
+    <h2><center><strong> Demande de services</strong></center></h2>    
+    <form method="POST" action="../PHP/valideAjoutDemandeServ.php">
+        <center>
+            <div class="form-row ">
+                <div class="form-group col-md-4">
+                    <label for="">Titre de la demande de service</label>
+                    <input type="text" class="form-control" id="titre" name="titre">
+                    <br>
+                    <label for="descriptionService"></label>
+                    <textarea id="Description" name="description"rows="4" cols="30">
+                            
+                    </textarea>
+                    <br>
+                    <br>
+                    <button type="submit" class="btn btn-primary" value="saveDemande" name="saveDemande">Valider la demande de Service</button>
+                </div>
+            </div>
+        </center>
+    </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </body>
+</body>
 </html>
